b46618c3a1a0b5760280c0d963841d4ce31a9e2b
karthik Yanagandula <kyanagandula@gmail.com>
2024-10-11 15:14:03 -0500

    gene sort solution

--- task/main.py
+++ task/main.py
@@ -8,6 +8,21 @@ import xmltodict
 import datetime
 from translate import Translator
 
+
+"""
+create a data model class for the data
+create a GET endpoint “/result” with sorted data returned in the following 4 categories in the order specified and each category sorted per sub bullet point
+genes with high risk
+same category, count the number of high risk conditions from high to low
+genes with high risk and inconclusive
+same category, count the number of high risk conditions from high to low
+genes with inconclusive but no high risk
+same category, count the number of inconclusive conditions from high to low
+genes with low risk only
+same category, count the number of low risk conditions from high to low
+although the data is small example, the code logic should work with arbitrary number of genes and arbitrary number of conditions
+"""
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -30,6 +45,89 @@ async def es_page_load(request: Request):
         data[0][idx]['es_title'] = translator.translate(item['title'])
     return templates.TemplateResponse(
         request=request, name="es_template.html", context={"data": data[0], "date": data[1]}
+    )
+
+@app.get("/result", response_class=HTMLResponse)
+async def result(request: Request):
+    data = [
+            {
+            "gene": "gene1",
+            "riskCategories": [
+            {"condition": "disease1", "risk": "high"},
+            {"condition": "disease2", "risk": "low"}
+            ]},
+            {
+            "gene": "gene2",
+            "riskCategories": [
+            {"condition": "disease3", "risk": "inconclusive"},
+            {"condition": "disease4", "risk": "low"}
+            ]},
+            {
+            "gene": "gene3",
+            "riskCategories": [
+            {"condition": "disease3", "risk": "high"},
+            {"condition": "disease2", "risk": "high"}
+            ]},
+            {
+            "gene": "gene4",
+            "riskCategories": [
+            {"condition": "disease3", "risk": "high"},
+            {"condition": "disease5", "risk": "inconclusive"}
+            ]},
+            {
+            "gene": "gene5",
+            "riskCategories": [
+            {"condition": "disease1", "risk": "inconclusive"}
+            ]},
+            {
+            "gene": "gene6",
+            "riskCategories": [
+            {"condition": "disease2", "risk": "low"}
+            ]}
+            ]
+    res = {}
+    
+    for d in data:
+        rankings = {"high": 0,
+                "inconclusive": 0,
+                "low": 0}
+        for risk in d['riskCategories']:
+            if risk['risk'] == 'high':
+                rankings[risk['risk']] += 1
+            elif risk['risk'] == 'inconclusive':
+                rankings[risk['risk']] += 1
+            else:
+                rankings[risk['risk']] += 1
+        res[d['gene']] = rankings
+    
+    high_risk = {}
+    high_and_inconclusive = {}
+    inconclusive_only = {}
+    low_risk_only = {}
+
+    for gene, conditions in res.items():
+        high = conditions['high']
+        inconclusive = conditions['inconclusive']
+        low = conditions['low']
+        
+        if high > 0:
+            if inconclusive > 0:
+                high_and_inconclusive[gene] = high
+            else:
+                high_risk[gene] = high
+        elif inconclusive > 0:
+            inconclusive_only[gene] = inconclusive
+        elif low > 0:
+            low_risk_only[gene] = low
+
+    # Sorting the categories based on the counts (from high to low)
+    high_risk_sorted = dict(sorted(high_risk.items(), key=lambda item: item[1], reverse=True))
+    high_and_inconclusive_sorted = dict(sorted(high_and_inconclusive.items(), key=lambda item: item[1], reverse=True))
+    inconclusive_only_sorted = dict(sorted(inconclusive_only.items(), key=lambda item: item[1], reverse=True))
+    low_risk_only_sorted = dict(sorted(low_risk_only.items(), key=lambda item: item[1], reverse=True))
+
+    return templates.TemplateResponse(
+        request=request, name="result_template.html", context={"result": [high_risk_sorted, high_and_inconclusive_sorted, inconclusive_only_sorted, low_risk_only_sorted]}
     )
 
 async def process_data():
